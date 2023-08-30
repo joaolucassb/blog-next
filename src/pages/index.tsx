e61ddcb1539c059903317ddc763643985739c43e
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-08-30 17:38:26 -0300

    add query string to getAllPosts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,9 @@ export default function Home({ posts }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getAllPosts();
+  const posts = await getAllPosts(
+    'sort=id:desc&pagination[start]=0&pagination[limit]=10',
+  );
 
   return {
     props: { posts },
