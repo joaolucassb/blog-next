be6399e18be8c1838ca37fc67d01ed407231c19b
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-09-14 13:20:45 -0300

    create PostCover Component

--- src/containers/Post/index.tsx
+++ src/containers/Post/index.tsx
@@ -2,6 +2,7 @@ import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 import { Heading } from '@/components/Heading';
 import { MainContainer } from '@/components/MainContainer';
+import { PostCover } from '@/components/PostCover';
 import { PostData } from '@/domain/posts/post';
 
 export type PostProps = {
@@ -14,6 +15,12 @@ export const Post = ({ post }: PostProps) => {
       <Header />
       <MainContainer>
         <Heading>{post.attributes.title}</Heading>
+        <PostCover
+          coverUrl={post.attributes.cover.data
+            .map((element) => element.attributes.formats.large.url)
+            .toString()}
+          alt={post.attributes.title}
+        />
         <div dangerouslySetInnerHTML={{ __html: post.attributes.content }} />
       </MainContainer>
       <Footer />
