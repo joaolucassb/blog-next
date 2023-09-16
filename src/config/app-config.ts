985cc8ace36eb75401752dc0017f4432830ffa22
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-09-16 13:13:06 -0300

    add url production

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://blog-strapi-app.onrender.com/api';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'MyBlog';
-export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+export const SITE_URL = 'https://myblognext.netlify.app';
